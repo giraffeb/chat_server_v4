8488becf98693566b03c87684b3aae567b31bd20
giraffeb <rwpakr07@gmail.com>
2019-08-28 19:22:08 +0900

    bug fix

--- src/redux/modules/socket_event.js
+++ src/redux/modules/socket_event.js
@@ -31,8 +31,13 @@ const socket_event = (state = initState, action)=>{
                 console.log('current chatroom not exist!');
                 return;
             }
+
             new_state.current_chatroom.chat_title = action.current_chatroom.chat_title;
-            new_state.current_chatroom.chat_list = action.current_chatroom.chat_list.slice();
+            
+            if(action.current_chatroom.chat_list !== null){
+                new_state.current_chatroom.chat_list = action.current_chatroom.chat_list.slice();
+            }
+            
 
             return new_state;
         default:
