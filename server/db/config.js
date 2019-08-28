f126f3cf91ffbd70899b627700bccc208ebfd0e0
giraffeb <rwpakr07@gmail.com>
2019-08-28 18:13:07 +0900

    db 정보 수정, 불필요 파일 삭제

--- server/db/config.js
+++ server/db/config.js
@@ -7,7 +7,8 @@ module.exports = function(){
     db.once('open', function(){
         console.log('db connect');
     })
-    
-    mongoose.connect('mongodb://localhost:/chatv2',{useNewUrlParser: true});
+    //mongodb+srv://soulgoon:<password>@cluster0-njgt7.azure.mongodb.net/test?retryWrites=true&w=majority
+    mongoose.connect('mongodb+srv://hello:hello@cluster0-njgt7.azure.mongodb.net/',{dbName: 'chatv2'});
+    // mongoose.connect('mongodb://localhost:/chatv2',{useNewUrlParser: true});
     return db;
 }
